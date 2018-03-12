007b9127a61d5d21f9486672dcf52828728720dd
Linda Zhao <lindajzhao@gmail.com>
2018-03-11 22:10:54 -0400

    now using google direction to search

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ pitstop.thiKey = "AIzaSyDCyp8JtEraKwveheT6vsFzLsG8e7UwG-Q"; //thi
 pitstop.lindaKey = "AIzaSyDzOkHWOtzDgG_6drpT4GbqgHwnTIApsTg"; //linda
 pitstop.joeyKey = "AIzaSyCYkPjAGfsJm2ow3qnk7HzHX0Q62oVdYiI"; // Joey
 pitstop.joey2Key = "AIzaSyChv5l9YHQ-H1rQdoq-mP3_1P1XEMwd3zY"; // Joey 2
-pitstop.currentKey = pitstop.joey2Key;
+pitstop.currentKey = pitstop.joeyKey;
 
 
 pitstop.userInputs = function () {
@@ -22,51 +22,149 @@ pitstop.userInputs = function () {
         const userStart = $('input[name=userStartPoint]').val();
         const userEnd = $('input[name=userEndPoint]').val();
 
+        // get coords from text search (promise)
         const start = pitstop.getCoordsFromTextSearch(userStart);
         const end =  pitstop.getCoordsFromTextSearch(userEnd);
 
-        $.when(start,end)
-            .then((startRes,endRes) => {
-                const startLat = startRes[0].results[0].geometry.location.lat;
-                const startLong = startRes[0].results[0].geometry.location.lng;
-                const endLat = endRes[0].results[0].geometry.location.lat;
-                const endLong = endRes[0].results[0].geometry.location.lng;
-                const coords = [startLat,startLong,endLat,endLong];
-                // use findMiddlePoint() to give us x and y coords on midpoint in array
-                const mid = pitstop.findMiddlePoint(coords);
-                // generates map centered at midpoint
-                initMap(mid);
-                // findLocationNearby() using midway coords
-                pitstop.locationNearby(mid);
+        const searchParam = 'duration';
+
+        // resolve promise
+        // $.when(start,end)
+        //     .then((startRes,endRes) => {
+        //         const startLat = startRes[0].results[0].geometry.location.lat;
+        //         const startLong = startRes[0].results[0].geometry.location.lng;
+        //         const endLat = endRes[0].results[0].geometry.location.lat;
+        //         const endLong = endRes[0].results[0].geometry.location.lng;
+
+        //         // make array of 4 coordinates
+        //         const coords = [startLat,startLong,endLat,endLong];
+        //         // use findMiddlePoint() to give us x and y coords on midpoint in array
+        //         const mid = pitstop.findMiddlePoint(coords);
+        //         // generates map centered at midpoint
+        //         // STRETCH: bounds on map        
+
+        const midpointPromise = pitstop.getDirections(userStart, userEnd);
+        $.when(midpointPromise)
+          .then((res) => {
+            console.log(res);
+            const bounds = res.routes[0].bounds
+            // km in route from start to end
+            const totalDistance = res.routes[0].legs[0].distance.value;
+            const halfwayDistance = totalDistance * 0.5;
+            //counter
+            let currentDistance = 0;
+            let placesPastHalf;
+            placesPastHalf = [];
+
+            res.routes[0].legs[0].steps.map((step) => {
+
+              const stepDistance = step.distance.value;
+              const startCoord = step.end_location;
+              currentDistance += stepDistance;
+              console.log(currentDistance);
+              if (currentDistance > halfwayDistance) {
+                placesPastHalf.push(startCoord);
+              }
             });
+
+            console.log(placesPastHalf);
+            const firstPlacePastHalf = placesPastHalf[0];
+            initMap(firstPlacePastHalf);
+            // findLocationNearby() using midway coords
+            pitstop.locationNearby(firstPlacePastHalf);
+          });
+          // .then((res) => {
+          //   // use time!!!!!
+          //   console.log(res, searchParam);
+          //   const bounds = res.routes[0].bounds
+          //   // km in route from start to end
+          //   const totalParam = res.routes[0].legs[0].searchParam.value;
+          //   console.log(totalParam)
+          //   const halfwayParam = totalParam * 0.5;
+          //   //counter
+          //   let currentParam = 0;
+          //   let placesPastHalf;
+          //   placesPastHalf = [];
+
+          //   res.routes[0].legs[0].steps.map((step) => {
+          //     const stepParam = step.searchParam.value;
+          //     const startCoord = step.start_location;
+          //     currentParam += stepParam;
+          //     console.log(currentParam);
+          //     if (currentParam > halfwayParam) {
+          //       placesPastHalf.push(startCoord);
+          //     }
+          //   });
+
+          //   console.log(placesPastHalf);
+          //   const firstPlacePastHalf = placesPastHalf[0];
+          //   initMap(firstPlacePastHalf);
+          //   // findLocationNearby() using midway coords
+          //   pitstop.locationNearby(firstPlacePastHalf);
+          // });
+
     });
 
 };
+
+// Decode an encoded levels string into an array of levels.
+// const encodedPolyline = "wejiGbjqcNDXDZFz@@FHh@D^N~APhBNdBTrBHfAJdAHx@Dl@Fp@Bd@DfABR@NBPDZ";
+// var decodedPath = google.maps.geometry.encoding.decodePath(encodedPolyline);
+// var decodedLevels = decodeLevels(encodedLevels);
+
+// pitstop.decodeLevels = (encodedLevelsString) => {
+//   var decodedLevels = [];
+
+//   for (var i = 0; i < encodedLevelsString.length; ++i) {
+//     var level = encodedLevelsString.charCodeAt(i) - 63;
+//     decodedLevels.push(level);
+//   }
+//   console.log(decodedLevels);
+//   return decodedLevels;
+// }
 
 // First ajax request will be pulling down information from google places through a text search that the user inputs.
+// Returns long and lat when given name of a place
 pitstop.getCoordsFromTextSearch = function (location) {
     return $.ajax({
-        url: "https://proxy.hackeryou.com",
-        method: "GET",
-        dataType: "json",
-        data: {
-            reqUrl:
-                "https://maps.googleapis.com/maps/api/place/textsearch/json",
-            params: {
-                key: pitstop.lindaKey,
-                query: location
-            }
-        }
+      url: "https://proxy.hackeryou.com",
+      method: "GET",
+      dataType: "json",
+      data: {
+          reqUrl:
+              "https://maps.googleapis.com/maps/api/place/textsearch/json",
+          params: {
+              key: pitstop.currentKey,
+              query: location
+          }
+      }
     });
 };
 
+pitstop.getDirections = (userStart, userEnd) => {
+    // search using english words
+  return $.ajax({
+    url: "https://proxy.hackeryou.com",
+    method: "GET",
+    dataType: "json",
+    data: {
+      reqUrl:
+        "https://maps.googleapis.com/maps/api/directions/json",
+      params: {
+        key: pitstop.lindaKey,
+        origin: userStart,
+        destination: userEnd,
+      }
+    }
+  });
+}
 
 pitstop.findMiddlePoint = function(coord) {
     /*
     * Find midpoint between two coordinates points
     * Source : http://www.movable-type.co.uk/scripts/latlong.html
     */
-
+    console.log(coord);
     //-- Define radius function
     if (typeof (Number.prototype.toRad) === "undefined") {
         Number.prototype.toRad = function () {
@@ -100,9 +198,28 @@ pitstop.findMiddlePoint = function(coord) {
         //-- Return result
         return [lng3.toDeg(), lat3.toDeg()];
     };
+
     return middlePoint(...coord);
 };
 
+// https://developers.google.com/maps/documentation/javascript/examples/polyline-simple
+pitstop.plotPolylines = (coordsArr) => {
+  // var flightPlanCoordinates = [
+  //   { lat: 37.772, lng: -122.214 },
+  //   { lat: 21.291, lng: -157.821 },
+  //   { lat: -18.142, lng: 178.431 },
+  //   { lat: -27.467, lng: 153.027 }
+  // ];
+
+  console.log(coordsArr);
+  var stepPath = new google.maps.Polyline({
+    path: coordsArr,
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  });
+}
 
 // First ajax request will be pulling down information from google places through a text search that the user inputs.
 pitstop.getCoordsFromTextSearch = function (location){
@@ -141,6 +258,7 @@ pitstop.getLocationDetails = function (place) {
 
 // BELOW IS HOW THE AJAX REQUEST WOULD LOOK FOR NEARBY SEARCH
 pitstop.locationNearby = function(geolocation) {
+  console.log(`${geolocation.lat},${geolocation.lng}`)
     $.ajax({
       url: "https://proxy.hackeryou.com",
       method: "GET",
@@ -150,18 +268,18 @@ pitstop.locationNearby = function(geolocation) {
           "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
         params: {
           key: pitstop.currentKey,
-          location: `${geolocation[1]},${geolocation[0]}`,
+          location: `${geolocation.lat},${geolocation.lng}`,
           radius: 1500,
           type: "restaurant"
         }
       }
     }).then(res => {
       const nearbyPlaces = res.results;
-
-        // Use the following functon to get the placeIds for each nearby place and send it to the LocationDetails function  
-        const nearbyPlacesPlaceIds = nearbyPlaces.map((item) => {
-            return (item.place_id);
-        });
+      console.log(res);
+      // Use the following function to get the placeIds for each nearby place and send it to the LocationDetails function  
+      const nearbyPlacesPlaceIds = nearbyPlaces.map((item) => {
+          return (item.place_id);
+      });
 
       // make array of promises from place details ajax call (full objects)
       const places = nearbyPlacesPlaceIds.map(pitstop.getLocationDetails);
@@ -187,21 +305,9 @@ pitstop.locationNearby = function(geolocation) {
           //   }
           // })();
 
-          const ratingSymbol = (() => {
-            if (rating) {
-              return `<p class="rating${rating} symbol" />`;
-            } else {
-              return "";
-            }
-          })();
-          // const ratingSymbol = rating ? `<p class="rating${rating} symbol" />` : "" ;
-          const priceSymbol = (() => {
-            if (priceLevel) {
-              return `<p class="price${priceLevel} symbol" />`;
-            } else {
-              return "";
-            }
-          })();
+          // Show rating and price only if it exists          
+          const ratingSymbol = rating ? `<p class="rating${rating} symbol" />` : "";
+          const priceSymbol = priceLevel ? `<p class="price${priceLevel} symbol" />` : "";
           
           $(".restoDetailsPanel").append(
             `<div class="restoDetailsItem">
@@ -256,7 +362,7 @@ pitstop.locationNearby = function(geolocation) {
     });
 };
 
-// // DISPLAY MARKERS FORMAT
+// DISPLAY MARKERS FORMAT
 // pitstop.hyCoords = { lat: 43.6484248, lng: -79.39792039999999 };
 
 pitstop.plotMarkers = function(coordArr) {
@@ -265,17 +371,6 @@ pitstop.plotMarkers = function(coordArr) {
         position: new google.maps.LatLng(coordArr[i][0], coordArr[i][1]),
         map: pitstop.Map
       });
-
-
-    // infowindow = new google.maps.InfoWindow();  
-    // google.maps.event.addListener(marker, "click", (function(marker, i) {
-    //       return function() {
-    //         infowindow.setContent('Test');
-    //         infowindow.open(pitstop.Map, marker);
-    //       };
-            // wahtever.show(this)
-    //     })(marker, i));
-
 
     infowindow = new google.maps.InfoWindow();  
     google.maps.event.addListener(marker, "click", (function(marker, i) {
@@ -289,14 +384,16 @@ pitstop.plotMarkers = function(coordArr) {
 };
 
 // https://developers.google.com/maps/documentation/javascript/adding-a-google-map
-function initMap(midpoint) {
-    const midpointObject = {
-        lat: midpoint[1],
-        lng: midpoint[0]
-    }
+function initMap(coords) {
+    // const midpointObject = {
+    //     lat: coords.lat,
+    //     lng: coords.lng
+    // }
+
+    console.log(coords);
     // create a map centered around the midpoint of route
     pitstop.Map = new google.maps.Map(document.getElementById("map"), {
-      center: midpointObject,
+      center: coords,
       zoom: 13
     });    
 }
