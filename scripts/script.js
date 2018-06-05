a34dd08ff5bb48d5d1ce3ffddebe7574d0855bde
Joey Deol <gurjothdeol@gmail.com>
2018-06-05 00:03:55 -0400

    Updated button styling to include hover and focus states. Also updated comments throughout entire application.

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ pitstop.thiKey = "AIzaSyDCyp8JtEraKwveheT6vsFzLsG8e7UwG-Q"; //thi
 pitstop.lindaKey = "AIzaSyDzOkHWOtzDgG_6drpT4GbqgHwnTIApsTg"; //linda
 pitstop.joeyKey = "AIzaSyCYkPjAGfsJm2ow3qnk7HzHX0Q62oVdYiI"; // Joey
 pitstop.joey2Key = "AIzaSyChv5l9YHQ-H1rQdoq-mP3_1P1XEMwd3zY"; // Joey 2
-pitstop.currentKey = pitstop.lindaKey;
+pitstop.currentKey = pitstop.joeyKey;
 
 
 pitstop.userInputs = function () {
@@ -29,17 +29,16 @@ pitstop.userInputs = function () {
     const midpointPromise = pitstop.getDirections(userStart, userEnd);
     $.when(midpointPromise)
       .then((res) => {
-        const bounds = res.routes[0].bounds
+        // const bounds = res.routes[0].bounds
         // km in route from start to end
         const totalDistance = res.routes[0].legs[0].distance.value;
         const halfwayDistance = totalDistance * 0.5;
-        //counter
+
+        // Step Counter for finding the first location past halfway
         let currentDistance = 0;
-        let placesPastHalf;
-        placesPastHalf = [];
+        const placesPastHalf = [];
 
         res.routes[0].legs[0].steps.map((step) => {
-
           const stepDistance = step.distance.value;
           const startCoord = step.end_location;
           currentDistance += stepDistance;
@@ -96,25 +95,6 @@ pitstop.getDirections = (userStart, userEnd) => {
   });
 }
 
-
-// First ajax request will be pulling down information from google places through a text search that the user inputs.
-// pitstop.getCoordsFromTextSearch = function (location) {
-//   return $.ajax({
-//     url: "https://proxy.hackeryou.com",
-//     method: "GET",
-//     dataType: "json",
-//     data: {
-//       reqUrl:
-//         "https://maps.googleapis.com/maps/api/place/textsearch/json",
-//       params: {
-//         key: pitstop.currentKey,
-//         query: location
-//       }
-//     }
-//   });
-// };
-
-
 // Create a function to request google place details api for the details of each location.
 pitstop.getLocationDetails = function (place) {
   return $.ajax({
@@ -132,7 +112,7 @@ pitstop.getLocationDetails = function (place) {
 
 };
 
-// BELOW IS HOW THE AJAX REQUEST WOULD LOOK FOR NEARBY SEARCH
+// Create a function to request google nearby search api for locations near the given halfway location.
 pitstop.locationNearby = function (geolocation) {
   $.ajax({
     url: "https://proxy.hackeryou.com",
@@ -157,7 +137,7 @@ pitstop.locationNearby = function (geolocation) {
       return (item.place_id);
     });
 
-    // make array of promises from place details ajax call (full objects)
+    // Make array of promises from place details ajax call (full objects)
     const places = nearbyPlacesPlaceIds.map(pitstop.getLocationDetails);
     $.when(...places).then((...placeArgs) => {
       placeArgs = placeArgs.map(el => el[0]);
@@ -165,7 +145,7 @@ pitstop.locationNearby = function (geolocation) {
       $(".landing.minimize").hide();
       $(".restoDetailsPanel").slideToggle();
 
-      // show the "Find my pitstops" button, only if it doesn't already exist
+      // Show the "Find my pitstops" button, only if it doesn't already exist
       if ($('.btn__showList').length === 0) {
         $("main").append(`<input type="submit" class="btn__showList" value="My Pitstops">`);
       }
@@ -215,7 +195,7 @@ pitstop.locationNearby = function (geolocation) {
             </a>
             </div>`);
 
-        // make array of long and lat
+        // Make array of long and lat
         const nearbyPlacesCoords = nearbyPlaces.map(item => {
           const nearbyPlaceLat = item.geometry.location.lat;
           const nearbyPlaceLong = item.geometry.location.lng;
@@ -224,11 +204,11 @@ pitstop.locationNearby = function (geolocation) {
           nearbyLatLong.push(nearbyPlaceLat, nearbyPlaceLong, nearbyPlaceName);
           return nearbyLatLong;
         });
-        // use long and lat to plot markers
+        // Use long and lat to plot markers
         pitstop.plotMarkers(nearbyPlacesCoords);
       });
 
-      // if there are no resturants at midpoint, append an error message
+      // If there are no resturants at midpoint, append an error message
       if (res.results.length === 0) {
         console.log('no restaurants found in the midpoint of this route! Please try again.')
         $(".restoDetailsPanel").append("<p>No restaurants found in the midpoint of this route! Please try again.</p>");
@@ -239,9 +219,7 @@ pitstop.locationNearby = function (geolocation) {
   });
 };
 
-// DISPLAY MARKERS FORMAT
-// pitstop.hyCoords = { lat: 43.6484248, lng: -79.39792039999999 };
-
+// Function used to create markers on the generated google map
 pitstop.plotMarkers = function (coordArr) {
   for (i = 0; i < coordArr.length; i++) {
     marker = new google.maps.Marker({
@@ -260,17 +238,13 @@ pitstop.plotMarkers = function (coordArr) {
   }
 };
 
-// https://developers.google.com/maps/documentation/javascript/adding-a-google-map
 function initMap(coords) {
-  // create a map centered around the midpoint of route
+  // Create a map centered around the midpoint of route
   pitstop.Map = new google.maps.Map(document.getElementById("map"), {
     center: coords,
     zoom: 13
   });
 }
-
-// https://developers.google.com/maps/documentation/javascript/directions#DisplayingResults
-
 
 pitstop.switchToMapView = () => {
   // .minimize triggers changes in css - show map only, move to top left
@@ -278,20 +252,21 @@ pitstop.switchToMapView = () => {
   $(".btn__startEnd").val('Recalculate');
 }
 
+// Application initializing function
 pitstop.init = function () {
   pitstop.userInputs();
   $('.restoDetailsPanel').hide();
 };
 
 pitstop.events = () => {
-  //when button is clicked show panel
+  // When button is clicked show panel
   $("main").on("click", ".btn__showList", () => {
     $(".restoDetailsPanel").slideToggle('slow');
     $('.landing.minimize').hide();
   });
 
   $('.btn__showForm').on('click', () => {
-    $('.landing.minimize').slideToggle(50);
+    $('.landing.minimize').slideToggle('slow');
     $(".restoDetailsPanel").hide();
   });
 
